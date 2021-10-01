bcc8578124d13771d000604debac353edbc503be
damushypotato <70552107+damushypotato@users.noreply.github.com>
2021-10-01 22:59:53 +1000

    intents in seperate file

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,61 +1,53 @@
-import { Client, Intents, Collection } from 'discord.js';
+import { Client, Collection } from 'discord.js';
+import { clientIntents } from './intents';
 import { connect as DB_Connect } from 'mongoose';
-import path from 'path';
-import fs from 'fs';
+import { join as joinPath } from 'path';
+import { readdirSync, existsSync } from 'fs';
 import { Command, Event, Config, Secrets } from '../Interfaces';
-import configJson from '../config.json';
+import * as configJson from '../config.json';
 import { config as envConfig } from 'dotenv';
 
-const devPath = path.join(__dirname, '..', '..', 'dev');
-if (fs.existsSync(devPath)) {
-    envConfig({ path: path.join(devPath, '.env'), });
+const devPath = joinPath(__dirname, '..', '..', 'dev');
+if (existsSync(devPath)) {
+    envConfig({ path: joinPath(devPath, '.env'), });
 }
 
 const secrets: Secrets = {
-    CLIENT_TOKEN: process.env.CLIENT_TOKEN,
-    MONGO_URI: process.env.MONGO_URI,
-    OWNER_ID: process.env.OWNER_ID,
-    API_KEYS: null
+    CLIENT_TOKEN: process.env.CLIENT_TOKEN as string,
+    MONGO_URI: process.env.MONGO_URI as string,
+    OWNER_ID: process.env.OWNER_ID as string,
+    API_KEYS: process.env.API_KEYS as string,
 };
 
 class ExtendedClient extends Client {
     public constructor() {
-        super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] })
+        super({ intents: clientIntents })
     }
 
     public commands: Collection<string, Command> = new Collection();
     public events: Collection<string, Event> = new Collection();
     public config: Config = configJson;
-    public secrets: Secrets = secrets;
+    public secrets = secrets;
 
     public async init() {
-        console.log('Attempting connection to MongoDB DataBase...')
-        try {
-            await DB_Connect(this.secrets.MONGO_URI);
-        }
-        catch (e) {
-            console.log('MongoDB DataBase connection error:', e)
-        }
-        console.log('Successfully connected to MongoDB DataBase.')
-        
         // Commands
-        const commandPath = path.join(__dirname, '..', 'Commands');
-        fs.readdirSync(commandPath).forEach(dir => {
-            const dirPath = path.join(commandPath, dir);
+        const commandPath = joinPath(__dirname, '..', 'Commands');
+        readdirSync(commandPath).forEach(dir => {
+            const dirPath = joinPath(commandPath, dir);
 
-            const commands = fs.readdirSync(dirPath).filter(file => file.endsWith('.ts'));
+            const commands = readdirSync(dirPath).filter(file => file.startsWith('c.'));
 
             for (const file of commands) {
-                const filePath = path.join(dirPath, file)
+                const filePath = joinPath(dirPath, file)
                 const { command } = require(filePath);
                 this.commands.set(command.name, command);
             }
         })
 
         // Events
-        const eventPath = path.join(__dirname, '..', 'Events');
-        fs.readdirSync(eventPath).filter(file => file.endsWith('.ts')).forEach(async file => {
-            const filePath = path.join(eventPath, file);
+        const eventPath = joinPath(__dirname, '..', 'Events');
+        readdirSync(eventPath).filter(file => file.startsWith('e.')).forEach(async file => {
+            const filePath = joinPath(eventPath, file);
             const { event } = await import(filePath);
             this.events.set(event.name, event);
             if (event.once) {
@@ -64,6 +56,15 @@ class ExtendedClient extends Client {
                 this.on(event.name, event.run.bind(null, this));
             }
         })
+
+        console.log('Attempting connection to MongoDB DataBase...')
+        try {
+            await DB_Connect(this.secrets.MONGO_URI);
+        }
+        catch (e) {
+            console.log('MongoDB DataBase connection error:', e)
+        }
+        console.log('Successfully connected to MongoDB DataBase.')
 
         // Login
         this.login(this.secrets.CLIENT_TOKEN);

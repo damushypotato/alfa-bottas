5670672c752689b9bdd29a9f01f803e34206f248
damushypotato <70552107+damushypotato@users.noreply.github.com>
2021-10-06 19:41:20 +1100

    #tag validation fix and other fixes

--- src/Modules/ClashRoyale/HashtagHelper.ts
+++ src/Modules/ClashRoyale/HashtagHelper.ts
@@ -3,7 +3,7 @@
 export namespace HashtagHelper {
     /**
      * Converts Hashtag (player or clantag) to a normalized version without # or common pitfalls
-     * @param hashtag Player- or clantag
+     * @param hashtag Player or clantag
      */
      export function normalizeHashtag(hashtag: string): string {
         if (hashtag == null) {
@@ -15,7 +15,7 @@ export namespace HashtagHelper {
             .toUpperCase()
             .replace(/O/g, '0'); // replace capital O with zero
 
-        if (hashtag.startsWith('#')) hashtag = hashtag.substring(1, hashtag.length - 1);
+        if (hashtag.startsWith('#')) hashtag = hashtag.substr(1, hashtag.length - 1);
 
         return hashtag;
     }
@@ -23,8 +23,9 @@ export namespace HashtagHelper {
     /**
      * Checks if a hashtag is potentially valid. Hashtags will be normalized before running through the test.
      * @param hashtag The to be checked hashtag
+     * @param skipNormalization Weather to skip the normalizing of the hashtag. Defaults to false
      */
-    export function isValidHashtag(hashtag: string): boolean {
+    export function isValidHashtag(hashtag: string, skipNormalization = false): boolean {
         // Simple validation first before doing computationally more expensive stuff
         if (hashtag === undefined || hashtag === null) {
             return false;
@@ -33,10 +34,11 @@ export namespace HashtagHelper {
             return false;
         }
 
-        const tagNormalized: string = normalizeHashtag(hashtag);
+        let tag = hashtag;
+        if (!skipNormalization) tag = normalizeHashtag(hashtag);
         const tagCharacters: string[] = ['0', '2', '8', '9', 'P', 'Y', 'L', 'Q', 'G', 'R', 'J', 'C', 'U', 'V'];
 
-        for (const char of tagNormalized) {
+        for (const char of tag) {
             if (tagCharacters.indexOf(char) === -1) {
                 return false;
             }

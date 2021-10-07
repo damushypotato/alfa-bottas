03f0de60d9ef8ccc5a04c3072047ed3274cb817f
damushypotato <70552107+damushypotato@users.noreply.github.com>
2021-10-07 15:01:04 +1100

    debug emoji commands

---
+++ src/SlashCommands/Debug/s.emoji.ts
@@ -0,0 +1,20 @@
+import {  } from 'discord.js';
+import { SlashCommand } from '../../Interfaces';
+
+export const slashCommand: SlashCommand = {
+    name: 'emoji',
+    description: 'sends an emoji from the bot\'s custom emojis',
+    type: 'CHAT_INPUT',
+    options: [
+        {
+            name: 'name',
+            type: 'STRING',
+            required: true,
+            description: 'The name of the emoji to send'
+        }
+    ],
+    async run(client, interaction, options, data) {
+        const emoji = options.getString('name');
+        interaction.followUp(client.customEmojis.get(emoji));
+    }
+}

d307e40c901251e5d246c12e8bffe291ce2666dd
damushypotato <70552107+damushypotato@users.noreply.github.com>
2021-10-23 02:42:59 +1100

    changeable emoji

--- src/Filters/f.sed.ts
+++ src/Filters/f.sed.ts
@@ -1,12 +1,14 @@
+import { Emoji } from 'discord.js';
 import { Filter } from '../Structures/Interfaces';
 
-export let id: string[] = ['890940402641829899'];
+export let ids: string[] = ['890940402641829899'];
+export let emoji: string = '💩';
 
 export const filter: Filter = {
     name: 'sed',
     enabled: true,
     async evaluate(client, message) {
-        if (id.includes(message.author.id)) message.react('💩');
+        if (ids.includes(message.author.id)) message.react(emoji);
         return true;
     },
 };

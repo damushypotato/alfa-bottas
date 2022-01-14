1b796cf243bfde0ed94458f8bd817f8a02618bfe
Mushy <k3.arjun@gmail.com>
2022-01-15 06:02:34 +1100

    cool...

--- src/Filters/cool.ts
+++ src/Filters/cool.ts
@@ -7,7 +7,7 @@ export const filter: Filter = {
     name: 'cool',
     enabled: true,
     async evaluate(client, message) {
-        if (ids.includes(message.author.id)) message.react('cool.');
+        if (ids.includes(message.author.id)) message.reply('cool.');
         return true;
     },
 };

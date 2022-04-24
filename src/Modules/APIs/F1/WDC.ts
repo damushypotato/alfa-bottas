ee17116acf038f1e825a8236b65aa46863e6fc38
Arjun K <k3.arjun@gmail.com>
2022-04-24 14:58:20 +1000

    remove cuz seb is 21st 😭

--- src/Modules/APIs/F1/WDC.ts
+++ src/Modules/APIs/F1/WDC.ts
@@ -8,13 +8,13 @@ export namespace WDC {
 
         const embed = client.newEmbed({
             title: `${wdc.season} Driver Standings (as of Round ${wdc.round})`,
-            fields: wdc.standings.map((d) => {
+            fields: wdc.standings.map(d => {
                 return {
-                    name: `\`${d.position}${
-                        d.position > 20 ? ' 😂' : ''
-                    }\` - ${client.customEmojis.get(d.constructors[0].id)} *${
-                        d.driver.firstName
-                    } ${d.driver.lastName}* ${d.driver.number}`,
+                    name: `\`${d.position}\` - ${client.customEmojis.get(
+                        d.constructors[0].id
+                    )} *${d.driver.firstName} ${d.driver.lastName}* ${
+                        d.driver.number
+                    }`,
                     value: `**${d.points}** point${d.points != 1 ? 's' : ''}${
                         d.wins > 0
                             ? ` | **${d.wins}** win${d.wins > 1 ? 's' : ''}`
